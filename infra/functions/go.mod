80f938964d890fd54632e16edcfdfc857c8e5dd4
Nhyl Bryle Ibañez <nbryleibanez@gmail.com>
2025-05-15 21:15:49 +0800

    wip

---
+++ infra/functions/go.mod
@@ -0,0 +1,5 @@
+module github.com/nbryleibanez/10k
+
+go 1.24.3
+
+require github.com/aws/aws-lambda-go v1.48.0 // indirect
---
+++ infra/lib/stateless/constructs/apigateway.ts
@@ -0,0 +1,6 @@
+export class ApiGatewayConstruct extends Construct {
+  constructor(scope: Construct: id: string) {
+    super(scope, id, props)
+
+  }
+}
